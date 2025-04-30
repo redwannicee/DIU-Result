21e8643fa910cd8ee9e75c4cd00a9ea0be03c88c
redwannicee <redwanrashid.nice@gmail.com>
2025-05-01 05:31:04 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = nextConfig
+module.exports = {
+  output: 'export',
+  basePath: isProd ? '/diu-cgpa' : '',
+  images: {
+    unoptimized: true,
+  },
+};
